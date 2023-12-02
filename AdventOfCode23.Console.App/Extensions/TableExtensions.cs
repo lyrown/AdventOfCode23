de41adf0231ce49cfac787b0aa2f4c83a30f125d
Julien <jscheffmann@mypoiworld.com>
2023-12-02 23:35:12 +0100

    Add project files.

---
+++ AdventOfCode23.Console.App/Extensions/TableExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode23.Console.App.Helpers
+{
+    using Spectre.Console;
+
+    public static class TableExtensions
+    {
+        public static void InsertDay(this Table table, int dayNumber, string solution)
+        {
+            var dayText = new Text($"{dayNumber}");
+            var bodyText = new Text(solution, new Style(Color.Blue, Color.Black));
+            table.AddRow(dayText, bodyText);
+        }
+    }
+}
---
+++ AdventOfCode23.Console.App/Program.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode23.Console.App.Helpers;
+using Spectre.Console;
+using Days = AdventOfCode23.Console.App.Solutions;
+
+AnsiConsole.Write(
+    new FigletText("Advent Of Code 23")
+        .Centered()
+        .Color(Color.Green));
+
+var table = new Table();
+table.ShowRowSeparators();
+table.AddColumn("Day");
+table.AddColumn("Solution");
+
+// Day 1
+var calibrationValue = Days.Day1.PuzzleSolution.GetPuzzleResponse();
+table.InsertDay(1, $"Total calibration value: {calibrationValue}");
+
+AnsiConsole.Write(table);
